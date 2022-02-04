9e33975afabbfaabd5c58a97059ff4742f1c1c08
Alicem Bülbül <alicem.buelbuel@outlook.com>
2022-02-04 10:43:00 +0100

    Title and Description

--- src/components.d.ts
+++ src/components.d.ts
@@ -10,7 +10,6 @@ declare module 'vue' {
     CarbonLogoGithub: typeof import('~icons/carbon/logo-github')['default']
     CarbonMoon: typeof import('~icons/carbon/moon')['default']
     CarbonSun: typeof import('~icons/carbon/sun')['default']
-    CarbonWarning: typeof import('~icons/carbon/warning')['default']
     Footer: typeof import('./components/Footer.vue')['default']
     Navigation: typeof import('./components/Navigation.vue')['default']
     README: typeof import('./components/README.md')['default']
